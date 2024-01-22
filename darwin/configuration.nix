50a1025512c56e3df6397ceb2a232c245be8c2f2
Mads Dam Hvidbjerg Eckardt <maeck@eg.dk>
2024-01-22 13:49:26 +0100

    more

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,7 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.lolcat ];
+  environment.systemPackages = [
+    pkgs.lolcat
+    pkgs.gnused
+    pkgs.gnugrep
+    pkgs.findutils
+    pkgs.cargo
+    pkgs.tmux
+    pkgs.tree
+    pkgs.ponysay
+    pkgs.cowsay
+    pkgs.curl
+    pkgs.fzf
+    pkgs.htop
+    pkgs.nano
+    pkgs.ripgrep
+    pkgs.kakoune
+    pkgs.ruby_2_7
+    pkgs.openssl_1_1
+    pkgs.openssl_1_1.dev
+    pkgs.nodejs_21
+    pkgs.python2
+    pkgs.zellij
+  ];
+
   programs.zsh.enable = true;
 
   services.postgresql = {
@@ -12,5 +35,6 @@
     script = "echo 'lolo'";
   };
   services.nix-daemon.enable = true;
+  nixpkgs.config.permittedInsecurePackages = [ "ruby-2.7.8" "openssl-1.1.1w" "python-2.7.18.7"];
   nixpkgs.config.allowUnfree = true;
 }
